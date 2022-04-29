c47101a13745d825c7a6e37fa04c4ca25de4e01d
xzx482 <xzx482@xzx482.ml>
2022-04-29 09:52:40 +0800

    11

---
+++ 扩展/例子_文件.py
@@ -0,0 +1,13 @@
+
+
+class 主():
+    def __init__(s,p):
+        s.p=p
+        s.消息=p.主消息.添加消息(1)
+
+    def ks(s):
+        pass
+
+def 配置(p,配置l):
+    a=主(p)
+    p.ks.connect(a.ks)
---
+++ 扩展/晨会提示.py
@@ -0,0 +1,15 @@
+
+class 主():
+    def __init__(s,p):
+        s.p=p
+        s.消息=p.主消息.添加消息(1)
+
+    def tqgx(s,*a):
+        print('12345',a)
+        s.p.日历.日期j
+        pass
+
+
+def 配置(p,配置l):
+    a=主(p)
+    p.天气.gxtq_signal.connect(a.tqgx)
