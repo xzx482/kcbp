b30fdf77d0475aa7b0671ea968b47175a737885c
xzx482 <1493440548@qq.com>
2022-07-07 17:54:22 +0800

    1

---
+++ 扩展/wd_/tl_.py
@@ -0,0 +1,40 @@
+
+from flj import flj
+import time
+
+def 获取天(t:int)->int:
+    tl=time.localtime(t)
+    return tl.tm_year*10000+tl.tm_mon*100+tl.tm_mday
+
+
+class tl_:
+    def __init__(s,f):
+        s.f:flj=f
+        s.f.添加默认值('tl',{'n':0,'t':0})
+        s.n=s.f['tl']['n']
+
+    def 修改(s,n):
+        s.n=n
+        s.f['tl']['n']=s.n
+        s.f['tl']['t']=time.time()
+        s.f.写_()
+    
+    def 加(s):
+        s.修改(s.n+1)
+
+    def 是否达到时间(s):
+        return 获取天(time.time())>获取天(s.f['tl']['t'])
+
+
+    def 标题(s):
+        return "高中英语听力宝典 提升训练下 模拟试题 "+str(s.n)+'\n第'+str(18+2*s.n)+'页'
+
+    def 标题_消息(s):
+        return '英语听力 第'+str(18+2*s.n)+'页'
+
+    def 内容_消息(s):
+        return "高中英语听力宝典 提升训练下 模拟试题 "+str(s.n)
+
+    def 文件名(s):
+        return r"D:\Desktop\英语\2022版听力宝典 提升训练下 成盘\2022版听力宝典 提升训练下 mp3 标准\0"+str(s.n+14)+" 2022版高中英语听力宝典 提升训练下  模拟试题 "+str(s.n)+".mp3"
+
